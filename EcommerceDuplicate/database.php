21a467b358c36f701208c774d5553eff9ee6c85c
Panashe77 <136387089+Panashe77@users.noreply.github.com>
2025-02-25 19:45:06 +0200

    Update database.php

--- EcommerceDuplicate/database.php
+++ EcommerceDuplicate/database.php
@@ -1,8 +1,8 @@
 <?php
 
 $host = "db"; // The service name for the MySQL container
-$username = "your_username"; // Replace with your actual MySQL username
-$password = "your_password"; // Replace with your actual MySQL password
+$username = "root"; // Replace with your actual MySQL username
+$password = ""; // Replace with your actual MySQL password
 $database = "exam"; // Your database name
 
 // Create a connection to MySQL server
